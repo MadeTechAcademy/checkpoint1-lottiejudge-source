fda096f1d7336f6eeed6ba06e9823434cbcefe75
lottie Judge <lottie.judge@madetech.com>
2025-11-24 11:21:06 +0000

    renaming test suit, chaning first test to check for correct duty length (13)

--- test_themes.py
+++ test_themes.py
@@ -1,5 +1,5 @@
 from themes import devops_apprenticeship_duty_list
 
-def testIt():
-    #assert len(x2)>10
-    assert True is True
+def test_duty_list():
+    assert len(devops_apprenticeship_duty_list) == 13
+    
